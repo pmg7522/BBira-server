82560dfebe5280ef9d52ffdd832c954e983fe1b3
chanyang217 <chanyang721@gmail.com>
2021-06-08 17:18:33 +0900

    Add app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json())
+app.use(cors({
+    origin: true,
+    credentials: true
+}))
+const port = 80;
+
+
+app.get('/', (req, res) => {
+    res.status(201).send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`서버가 ${port}번에서 작동중입니다.`);
+});
