085ce565f24e03ab16d1d7399106278a1326b749
pmg7522 <pmg752290@gmail.com>
2021-06-09 11:17:43 +0900

    Fix logout.js

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = (req, res) => {
+    serverLogout: (req, res) => {
     req.session.destroy();
     res.status(200).send("ok");
+    }
 }
