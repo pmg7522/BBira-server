2a27437745f4866a52e6c6813db74c88329c774a
pmg7522 <pmg752290@gmail.com>
2021-06-09 11:25:24 +0900

    Fix login.js

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,19 +1,17 @@
-const { Users } = require('../../models');
+const {  } = require('../../models');
 const jwt = require('jsonwebtoken');
 const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = (req, res) => {
-    serverLogin: async (req, res) => {
-        const userInfo = await Users.findOne({
-          where: { email: req.body.email, password: req.body.password }
-        })
+    const userInfo = await .findOne({
+      where: { email: req.body.email, password: req.body.password }
+    })
         
-        if (!userInfo) {
-          res.status(404).send("invalid user");
-        } else {
-          req.session.userId = userInfo.id;
-          res.status(200).send("ok");
-       }
+    if (!userInfo) {
+      res.status(404).send("invalid user");
+    } else {
+      req.session.userId = userInfo.id;
+      res.status(200).send("ok");
     }
 }
