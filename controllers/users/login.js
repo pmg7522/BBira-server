728b6c84477037797ad6aba7fd50322abde0d984
pmg7522 <pmg752290@gmail.com>
2021-06-09 11:14:40 +0900

    Feat login.js

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -4,7 +4,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = (req, res) => {
-
-
-    
+    serverLogin: async (req, res) => {
+        const userInfo = await Users.findOne({
+          where: { email: req.body.email, password: req.body.password }
+        })
+        
+        if (!userInfo) {
+          res.status(404).send("invalid user");
+        } else {
+          req.session.userId = userInfo.id;
+          res.status(200).send("ok");
+       }
+    }
 }
