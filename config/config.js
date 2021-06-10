351957ae5db9a4a7c55aaa082ff0a9616fce2411
chanyang217 <chanyang721@gmail.com>
2021-06-10 16:03:13 +0900

    Fix Port Errer

--- config/config.js
+++ config/config.js
@@ -20,6 +20,7 @@ module.exports = {
     "password": process.env.DATABASE_PASSWORD,
     "database": "BBira",
     "host": process.env.DATABASE_HOST,
+    "port": 13306,
     "dialect": "mysql"
   }
 }
--- schema.sql
+++ schema.sql
@@ -3,38 +3,49 @@ CREATE TABLE users (
   nickname varchar(255) NOT NULL,
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
-  store_id INT AUTO_INCREMENT,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  store_id INT,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE store (
+CREATE TABLE stores (
   id INT AUTO_INCREMENT,
   phone varchar(255) NOT NULL,
   storename varchar(255) NOT NULL,
   address varchar(255) NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE item (
+CREATE TABLE items (
   id INT AUTO_INCREMENT,
   itemname varchar(255) NOT NULL,
   itemphoto varchar(255) NOT NULL,
   itemdesc varchar(255) NOT NULL,
   itemprice varchar(255) NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   store_id INT,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE tag (
+CREATE TABLE tags (
     id INT AUTO_INCREMENT,
     tagname varchar(255) NOT NULL,
+    createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
 
 
-CREATE TABLE tag_store (
+CREATE TABLE tag_stores (
+    id INT AUTO_INCREMENT,
     tag_id INT,
-    store_id INT 
+    store_id INT,
+    createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
 
 
@@ -47,7 +58,7 @@ ALTER TABLE tag_stores ADD FOREIGN KEY (tag_id) REFERENCES tags (id);
 ALTER TABLE stores CHANGE COLUMN createdAt createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE stores CHANGE COLUMN updatedAt updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
-INSERT INTO users (email, nickname, password, store_id) VALUES ("BBira@gmail.com", "BBira", "1234", 1);
+INSERT INTO users (email, nickname, password) VALUES ("BBira@gmail.com", "BBira", "1234");
 INSERT INTO stores (phone, storename, address) VALUES ("010-1234-5678", "shop", "강북");
 -- INSERT INTO tags (tagname) VALUES ()
 -- INSERT INTO items (itemname, itemphoto, itemdesc, itemprice) VALUES ()
